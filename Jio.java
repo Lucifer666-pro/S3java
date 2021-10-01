389fed8941e4d352e4897ea2e9eefa80d2a70e8d
Mashmallo-007 <91688891+Mashmallo-007@users.noreply.github.com>
2021-10-01 16:33:33 +0530

    Revert "Update Jio.java"

--- Jio.java
+++ Jio.java
@@ -1,8 +1,6 @@
 package Javapack;
 public class Jio{
-public static void main(String array[]){
-
-  System.out.println("It is an example of java package");
-  System.out.println("The program successful executed");
+public static void main(String arr[]){
+System.out.println("hi");
 }
 }
